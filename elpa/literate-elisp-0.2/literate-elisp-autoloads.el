e39a3da9d459e54f0d3ee1db9528a0e9a2eaefeb
Richard Trotta <trotta@cua.edu>
2019-10-16 13:13:41 -0400

    Removed melpa because not in ifarm version of emacs (i.e. 23 instead of 24)

--- elpa/literate-elisp-0.2/literate-elisp-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; literate-elisp-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("literate-elisp.el") (23593 1400 324222
-;;;;;;  631000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; literate-elisp-autoloads.el ends here
--- elpa/org-pdfview-0.4/org-pdfview-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "org-pdfview" "0.4" "Support for links to documents in pdfview mode" '((org "6.1") (pdf-tools "0.40")) :commit "3a96bfb57cb158ac02cfb4225512699c66f5221d" :keywords '("org" "pdf-view" "pdf-tools") :authors '(("Markus Hauck" . "markus1189@gmail.com")) :maintainer '("Markus Hauck" . "markus1189@gmail.com"))
